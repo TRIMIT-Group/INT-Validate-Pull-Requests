c292570496a9628fd85cff6c888a59f76d1b3fe9
Kristian Ruseng Frandsen <kef@cornator.com>
2023-03-03 17:02:01 +0100

    Progress

--- dist/index.js
+++ dist/index.js
@@ -45,12 +45,26 @@ async function runValidation() {
             // Check if branch is heading in the right direction
             switch(payload.base.ref) {
                 case branchDevelop: // if towards develop branch
+                    core.info(`Validating rules for base branch: ${branchDevelop}`)
                     if (!branchName.startsWith(prefixFeature) && !branchName.startsWith(prefixAlign)) {
-                        core.setFailed(`Pull Request towards ${branchName} branch requires. Did not match any of the allowed prefixes: ${prefixFeature}, ${prefixHotfix}, ${prefixAlign}`);
+                        core.setFailed(`Pull request from ${branchName} to ${branchDevelop} denied. Reason: Invalid branch prefix. Allowed prefixes: ${prefixFeature}, ${prefixAlign}`);
                         return;
                     }
+                    break;
                 case branchMain: // if towards main branch
-                    
+                    core.info(`Validating rules for base branch: ${branchMain}`)
+                    if (branchName != branchDevelop && !branchName.startsWith(prefixHotfix)) {
+                        core.setFailed(`Pull request from ${branchName} to ${branchMain} denied. Reason: Invalid branch prefix. Allowed prefixes: ${prefixHotfix}`);
+                        return;
+                    }
+                    break;
+            }
+        }
+        else if (branchName == branchDevelop) { // head branch is the develop branch
+            core.info(`Validating rules for using ${branchDevelop} as head branch.`)
+            if (payload.base.ref != branchMain) {
+                core.setFailed(`Pull request from ${branchName} is only allowed with ${branchMain} as base branch.`);
+                return;
             }
         }
     } catch (error) {
